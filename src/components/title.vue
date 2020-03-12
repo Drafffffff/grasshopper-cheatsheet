f7022170809d4f3a10464aa30114f60bf171cab5
drafffffff <i0.0ia@qq.com>
2020-03-12 23:11:34 +0800

    back

---
+++ src/components/title.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="">
+      <p class="text-4xl pt-8 text-gray-800 text-center pb-6 align-middle">
+        Grasshopper
+        <span class="text-gray-600">
+          Cheatsheet
+        </span>
+      </p>
+
+      <div class="text-center text-base text-gray-800">
+        <p>GH快速上手</p>
+
+        <!-- <p class="text-xs">Powered by <a class="underline text-gray-600" href="https://drafff.art">Drafff</a></p> -->
+      </div>
+
+      <div class="container  mx-auto mt-6 sm:max-w-xl md:max-w-4xl text-gray-800">
+        <p class="text-justify px-6 text-xs ">
+          Grasshopper（简称GH）是一款可视化编程语言，它基于Rhino平台运行，是数据化设计方向的主流软件之一，同时与交互设计也有重叠的区域。
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
